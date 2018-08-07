1d608b126ba6a8fb0c9734fde5d36b832ecdb289
jose arjona <josedarjona@gmail.com>
2018-08-07 15:24:58 -0400

    hackerrank left rotation

---
+++ left-rotation/left-rotation.js
@@ -0,0 +1,39 @@
+// Output Format
+
+// Print a single line of n space-separated integers 
+// denoting the final state of the array after performing d left rotations.
+
+// Sample Input
+
+// 5 4
+// 1 2 3 4 5
+// Sample Output
+
+// 5 1 2 3 4
+
+
+
+
+function rotLeft(a, d) {
+
+    
+  let fillArr = a
+  let i = 0
+  while(i < d){
+      
+      // fillArr = a.slice(1)
+      // fillArr.push(a[0])
+      let first = fillArr.shift();
+      fillArr.push(first);
+      
+      
+      // a = fillArr
+      // fillArr = []
+      
+      i++
+      
+  }
+
+  return fillArr
+
+}
