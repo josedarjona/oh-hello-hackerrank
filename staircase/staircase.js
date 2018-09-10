35b02b3529ce9917f5fbad246f78f8fbb680f5d2
jose arjona <josedarjona@gmail.com>
2018-09-10 02:53:13 -0400

    adjust

--- staircase/staircase.js
+++ staircase/staircase.js
@@ -1,14 +1,3 @@
-// Consider a staircase of size n:4
-
-//    #
-//   ##
-//  ###
-// ####
-// Observe that its base and height are both equal to , and 
-// the image is drawn using # symbols and spaces.
-//  The last line is not preceded by any spaces.
-
-// Write a program that prints a staircase of size .
 
 
 
@@ -31,3 +20,16 @@ function staircase(n) {
   
 
 }
+
+
+// Consider a staircase of size n:4
+
+//    #
+//   ##
+//  ###
+// ####
+// Observe that its base and height are both equal to , and 
+// the image is drawn using # symbols and spaces.
+//  The last line is not preceded by any spaces.
+
+// Write a program that prints a staircase of size .
